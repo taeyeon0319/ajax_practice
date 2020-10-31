2d9e4d90a933cbbf8ab36d3886c5d9a281ffca4b
ygk313 <50168972+ygk313@users.noreply.github.com>
2020-10-31 14:31:27 +0900

    dislike model 추가

--- items/models.py
+++ items/models.py
@@ -10,11 +10,16 @@ class Post(models.Model):
     image = models.ImageField(upload_to='images/', null=True)
     user = models.ForeignKey(User,on_delete=models.CASCADE,null=True)
     like_user_set = models.ManyToManyField(User, blank=True, related_name='likes_user_set',through='Like')
+    dislike_user_set = models.ManyToManyField(User, blank=True, related_name='dislikes_user_set',through='Dislike')
 
     @property
     def like_count(self):
         return self.like_user_set.count()
 
+    @property
+    def dislike_count(self):
+        return self.dislike_user_set.count()
+        
 class Comment(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
@@ -31,3 +36,13 @@ class Like(models.Model):
 
     class Meta:
         unique_together =(('user', 'post'))
+
+#싫어요 모델
+class Dislike(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        unique_together = (('user', 'post'))
